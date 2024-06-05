f60cde0df96eaac269a7da2d58767a7d6d6236d5
TahinaBemax <153293127+TahinaBemax@users.noreply.github.com>
2024-06-05 16:16:16 +0300

    Schema  base de données farany
     tsy azo ovaina intsony.

---
+++ database/schema3.1.sql
@@ -0,0 +1,156 @@
+CREATE TABLE Utilisateur(
+   id_utilisateur SERIAL,
+   nom VARCHAR(255)  NOT NULL,
+   prenom VARCHAR(255)  NOT NULL,
+   date_naissance DATE NOT NULL CHECK(date_naissance < CURRENT_DATE),
+   adresse VARCHAR(255) ,
+   mail VARCHAR(255)  NOT NULL,
+   etat_civil VARCHAR(255) ,
+   photo VARCHAR(255)  NOT NULL,
+   point BIGINT NOT NULL DEFAULT 0 CHECK(point >= 0 ),
+   latitude NUMERIC(15,2)  ,
+   longitude NUMERIC(15,2)  ,
+   motdepasse VARCHAR(255)  NOT NULL,
+   PRIMARY KEY(id_utilisateur)
+);
+
+CREATE TABLE Secteur(
+   id_secteur SERIAL,
+   secteur VARCHAR(255)  NOT NULL,
+   PRIMARY KEY(id_secteur)
+);
+
+CREATE TABLE Diplome(
+   id_diplome SERIAL,
+   diplome VARCHAR(255)  NOT NULL,
+   niveau INTEGER NOT NULL,
+   id_secteur INTEGER NOT NULL,
+   PRIMARY KEY(id_diplome),
+   UNIQUE(id_secteur),
+   FOREIGN KEY(id_secteur) REFERENCES Secteur(id_secteur)
+);
+
+CREATE TABLE Entreprise(
+   id_entreprise SERIAL,
+   entreprise VARCHAR(255)  NOT NULL,
+   point BIGINT NOT NULL DEFAULT 0 CHECK(point >= 0 ),
+   type VARCHAR(255) ,
+   date_creation DATE NOT NULL,
+   adresse VARCHAR(255)  NOT NULL,
+   site_web VARCHAR(255) ,
+   mail VARCHAR(255)  NOT NULL,
+   latitude NUMERIC(15,2)  ,
+   longitude NUMERIC(15,2)  ,
+   image VARCHAR(255)  NOT NULL,
+   PRIMARY KEY(id_entreprise)
+);
+
+CREATE TABLE poste(
+   id_poste SERIAL,
+   description VARCHAR(255) ,
+   date_insertion DATE NOT NULL DEFAULT CURRENT_DATE,
+   salaire NUMERIC(15,2)   NOT NULL CHECK(salaire >= 0)  DEFAULT 0,
+   titre VARCHAR(255)  NOT NULL,
+   annee_experience INTEGER DEFAULT 0,
+   annee_formation INTEGER DEFAULT 0,
+   age_min INTEGER,
+   age_max INTEGER,
+   id_diplome INTEGER NOT NULL,
+   id_entreprise INTEGER NOT NULL,
+   PRIMARY KEY(id_poste),
+   FOREIGN KEY(id_diplome) REFERENCES Diplome(id_diplome),
+   FOREIGN KEY(id_entreprise) REFERENCES Entreprise(id_entreprise)
+);
+
+CREATE TABLE competence(
+   id_competence SERIAL,
+   competence VARCHAR(255)  NOT NULL,
+   description TEXT,
+   id_utilisateur INTEGER NOT NULL,
+   PRIMARY KEY(id_competence),
+   FOREIGN KEY(id_utilisateur) REFERENCES Utilisateur(id_utilisateur)
+);
+
+CREATE TABLE experience(
+   id_experiecne SERIAL,
+   date_debut DATE NOT NULL,
+   date_fin DATE,
+   description TEXT NOT NULL,
+   id_utilisateur INTEGER NOT NULL,
+   PRIMARY KEY(id_experiecne),
+   FOREIGN KEY(id_utilisateur) REFERENCES Utilisateur(id_utilisateur)
+);
+
+CREATE TABLE Formation(
+   id_formation SERIAL,
+   date_debut DATE NOT NULL,
+   date_fin DATE,
+   description TEXT NOT NULL,
+   id_utilisateur INTEGER NOT NULL,
+   PRIMARY KEY(id_formation),
+   FOREIGN KEY(id_utilisateur) REFERENCES Utilisateur(id_utilisateur)
+);
+
+CREATE TABLE Notification(
+   id_notification SERIAL,
+   message TEXT NOT NULL,
+   date_notification TIMESTAMP NOT NULL DEFAULT CURRRENT_DATE,
+   date_lu TIMESTAMP DEFAULT CURRENT_DATE,
+   id_utilisateur INTEGER NOT NULL,
+   PRIMARY KEY(id_notification),
+   FOREIGN KEY(id_utilisateur) REFERENCES Utilisateur(id_utilisateur)
+);
+
+CREATE TABLE Question(
+   id_question SERIAL,
+   question TEXT NOT NULL,
+   PRIMARY KEY(id_question)
+);
+
+CREATE TABLE Reponse(
+   id_reponse SERIAL,
+   reponse TEXT NOT NULL,
+   id_question INTEGER NOT NULL,
+   PRIMARY KEY(id_reponse),
+   FOREIGN KEY(id_question) REFERENCES Question(id_question)
+);
+
+CREATE TABLE type_notification(
+   id_notification SERIAL,
+   PRIMARY KEY(id_notification)
+);
+
+CREATE TABLE Contact(
+   id_contact SERIAL,
+   num_telephone VARCHAR(20) ,
+   id_entreprise INTEGER NOT NULL,
+   id_utilisateur INTEGER NOT NULL,
+   PRIMARY KEY(id_contact),
+   UNIQUE(num_telephone),
+   FOREIGN KEY(id_entreprise) REFERENCES Entreprise(id_entreprise),
+   FOREIGN KEY(id_utilisateur) REFERENCES Utilisateur(id_utilisateur)
+);
+
+CREATE TABLE diplome_utilisateur(
+   id_utilisateur INTEGER,
+   id_diplome INTEGER,
+   PRIMARY KEY(id_utilisateur, id_diplome),
+   FOREIGN KEY(id_utilisateur) REFERENCES Utilisateur(id_utilisateur),
+   FOREIGN KEY(id_diplome) REFERENCES Diplome(id_diplome)
+);
+
+CREATE TABLE Asso_11(
+   id_notification INTEGER,
+   id_notification_1 INTEGER,
+   PRIMARY KEY(id_notification, id_notification_1),
+   FOREIGN KEY(id_notification) REFERENCES Notification(id_notification),
+   FOREIGN KEY(id_notification_1) REFERENCES type_notification(id_notification)
+);
+
+CREATE TABLE Asso_12(
+   id_secteur INTEGER,
+   id_competence INTEGER,
+   PRIMARY KEY(id_secteur, id_competence),
+   FOREIGN KEY(id_secteur) REFERENCES Secteur(id_secteur),
+   FOREIGN KEY(id_competence) REFERENCES competence(id_competence)
+);
