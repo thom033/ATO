e6e1d2be9745ef4ccb9fd10ba5f144819d772448
ainamanampisoa <169085336+ainamanampisoa@users.noreply.github.com>
2024-06-18 13:31:11 +0300

    commit done

---
+++ src/main/java/itu/poste/PosteDetail.java
@@ -0,0 +1,229 @@
+package itu.poste;
+public class PosteDetail {
+    private Long idPoste;
+    private String posteDescription;
+    private String posteDateInsertion;
+    private Double posteSalaire;
+    private String posteTitre;
+    private Integer nbrAnneeExperience;
+    private Integer nbrAnneeFormation;
+    private Integer ageRequise;
+    private Long idDiplome;
+    private String diplomeNom;
+    private Long idSecteur;
+    private String secteurNom;
+    private Long idEntreprise;
+    private String entrepriseNom;
+    private Integer entreprisePoint;
+    private String entrepriseType;
+    private String entrepriseDateCreation;
+    private String entrepriseAdresse;
+    private String entrepriseSiteWeb;
+    private String entrepriseMail;
+    private String entrepriseNumTelephone;
+    private Double entrepriseLatitude;
+    private Double entrepriseLongitude;
+    private String entrepriseImage;
+    private String competenceRequise;
+
+    // Getters and Setters
+    public Long getIdPoste() {
+        return idPoste;
+    }
+
+    public void setIdPoste(Long idPoste) {
+        this.idPoste = idPoste;
+    }
+
+    public String getPosteDescription() {
+        return posteDescription;
+    }
+
+    public void setPosteDescription(String posteDescription) {
+        this.posteDescription = posteDescription;
+    }
+
+    public String getPosteDateInsertion() {
+        return posteDateInsertion;
+    }
+
+    public void setPosteDateInsertion(String posteDateInsertion) {
+        this.posteDateInsertion = posteDateInsertion;
+    }
+
+    public Double getPosteSalaire() {
+        return posteSalaire;
+    }
+
+    public void setPosteSalaire(Double posteSalaire) {
+        this.posteSalaire = posteSalaire;
+    }
+
+    public String getPosteTitre() {
+        return posteTitre;
+    }
+
+    public void setPosteTitre(String posteTitre) {
+        this.posteTitre = posteTitre;
+    }
+
+    public Integer getNbrAnneeExperience() {
+        return nbrAnneeExperience;
+    }
+
+    public void setNbrAnneeExperience(Integer nbrAnneeExperience) {
+        this.nbrAnneeExperience = nbrAnneeExperience;
+    }
+
+    public Integer getNbrAnneeFormation() {
+        return nbrAnneeFormation;
+    }
+
+    public void setNbrAnneeFormation(Integer nbrAnneeFormation) {
+        this.nbrAnneeFormation = nbrAnneeFormation;
+    }
+
+    public Integer getAgeRequise() {
+        return ageRequise;
+    }
+
+    public void setAgeRequise(Integer ageRequise) {
+        this.ageRequise = ageRequise;
+    }
+
+    public Long getIdDiplome() {
+        return idDiplome;
+    }
+
+    public void setIdDiplome(Long idDiplome) {
+        this.idDiplome = idDiplome;
+    }
+
+    public String getDiplomeNom() {
+        return diplomeNom;
+    }
+
+    public void setDiplomeNom(String diplomeNom) {
+        this.diplomeNom = diplomeNom;
+    }
+
+    public Long getIdSecteur() {
+        return idSecteur;
+    }
+
+    public void setIdSecteur(Long idSecteur) {
+        this.idSecteur = idSecteur;
+    }
+
+    public String getSecteurNom() {
+        return secteurNom;
+    }
+
+    public void setSecteurNom(String secteurNom) {
+        this.secteurNom = secteurNom;
+    }
+
+    public Long getIdEntreprise() {
+        return idEntreprise;
+    }
+
+    public void setIdEntreprise(Long idEntreprise) {
+        this.idEntreprise = idEntreprise;
+    }
+
+    public String getEntrepriseNom() {
+        return entrepriseNom;
+    }
+
+    public void setEntrepriseNom(String entrepriseNom) {
+        this.entrepriseNom = entrepriseNom;
+    }
+
+    public Integer getEntreprisePoint() {
+        return entreprisePoint;
+    }
+
+    public void setEntreprisePoint(Integer entreprisePoint) {
+        this.entreprisePoint = entreprisePoint;
+    }
+
+    public String getEntrepriseType() {
+        return entrepriseType;
+    }
+
+    public void setEntrepriseType(String entrepriseType) {
+        this.entrepriseType = entrepriseType;
+    }
+
+    public String getEntrepriseDateCreation() {
+        return entrepriseDateCreation;
+    }
+
+    public void setEntrepriseDateCreation(String entrepriseDateCreation) {
+        this.entrepriseDateCreation = entrepriseDateCreation;
+    }
+
+    public String getEntrepriseAdresse() {
+        return entrepriseAdresse;
+    }
+
+    public void setEntrepriseAdresse(String entrepriseAdresse) {
+        this.entrepriseAdresse = entrepriseAdresse;
+    }
+
+    public String getEntrepriseSiteWeb() {
+        return entrepriseSiteWeb;
+    }
+
+    public void setEntrepriseSiteWeb(String entrepriseSiteWeb) {
+        this.entrepriseSiteWeb = entrepriseSiteWeb;
+    }
+
+    public String getEntrepriseMail() {
+        return entrepriseMail;
+    }
+
+    public void setEntrepriseMail(String entrepriseMail) {
+        this.entrepriseMail = entrepriseMail;
+    }
+
+    public String getEntrepriseNumTelephone() {
+        return entrepriseNumTelephone;
+    }
+
+    public void setEntrepriseNumTelephone(String entrepriseNumTelephone) {
+        this.entrepriseNumTelephone = entrepriseNumTelephone;
+    }
+
+    public Double getEntrepriseLatitude() {
+        return entrepriseLatitude;
+    }
+
+    public void setEntrepriseLatitude(Double entrepriseLatitude) {
+        this.entrepriseLatitude = entrepriseLatitude;
+    }
+
+    public Double getEntrepriseLongitude() {
+        return entrepriseLongitude;
+    }
+
+    public void setEntrepriseLongitude(Double entrepriseLongitude) {
+        this.entrepriseLongitude = entrepriseLongitude;
+    }
+
+    public String getEntrepriseImage() {
+        return entrepriseImage;
+    }
+
+    public void setEntrepriseImage(String entrepriseImage) {
+        this.entrepriseImage = entrepriseImage;
+    }
+
+    public String getCompetenceRequise() {
+        return competenceRequise;
+    }
+
+    public void setCompetenceRequise(String competenceRequise) {
+        this.competenceRequise = competenceRequise;
+    }
+}
--- src/main/java/itu/recherche/RechercheController.java
+++ src/main/java/itu/recherche/RechercheController.java
@@ -1,13 +1,16 @@
 package itu.recherche;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.sql.Timestamp;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -16,7 +19,9 @@ import itu.competence.Competence;
 import itu.competence.CompetenceRepository;
 import itu.diplome.Diplome;
 import itu.diplome.DiplomeRepository;
-import itu.secteur.*;
+import itu.secteur.Secteur;
+import itu.secteur.SecteurRepository;
+import itu.poste.PosteDetail;
 
 @Controller
 public class RechercheController {
@@ -33,7 +38,7 @@ public class RechercheController {
     JdbcTemplate jdbcTemplate;
 
     public RechercheController(SecteurRepository secteurRepository, DiplomeRepository diplomeRepository,
-            CompetenceRepository competenceRepository, JdbcTemplate jdbcTemplate) {
+                                CompetenceRepository competenceRepository, JdbcTemplate jdbcTemplate) {
         this.secteurRepository = secteurRepository;
         this.diplomeRepository = diplomeRepository;
         this.competenceRepository = competenceRepository;
@@ -55,7 +60,7 @@ public class RechercheController {
     }
 
     @GetMapping("/recherche_result")
-    public String searchResult(@RequestParam Map<String, String> params, Model model) {
+    public List<PosteDetail> searchResult(@RequestParam Map<String, String> params) {
         String title = params.get("title");
         String diplome = params.get("diplome");
         String secteur = params.get("secteur");
@@ -66,10 +71,10 @@ public class RechercheController {
         String salaireMax = params.get("salaireMax");
         String distance = params.get("distance");
         String anneeExperience = params.get("anneeExperience");
-    
+
         StringBuilder sql = new StringBuilder("SELECT * FROM postes_details WHERE 1=1");
         List<Object> paramsList = new ArrayList<>();
-    
+
         if (title != null && !title.isEmpty()) {
             sql.append(" AND poste_titre LIKE ?");
             paramsList.add("%" + title + "%");
@@ -110,12 +115,53 @@ public class RechercheController {
             sql.append(" AND nbr_annee_experience >= ?");
             paramsList.add(Integer.parseInt(anneeExperience));
         }
-    
-        List<Map<String, Object>> results = jdbcTemplate.queryForList(sql.toString(), paramsList.toArray());
-        model.addAttribute("results", results);
-    
-        return "recherche/resultat";
+
+        List<Map<String, Object>> rows = jdbcTemplate.queryForList(sql.toString(), paramsList.toArray());
+        List<PosteDetail> results = new ArrayList<>();
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+        for (Map<String, Object> row : rows) {
+            PosteDetail posteDetail = new PosteDetail();
+            posteDetail.setIdPoste((Long) row.get("id_poste"));
+            posteDetail.setPosteDescription((String) row.get("poste_description"));
+
+            // Conversion de java.sql.Date et java.sql.Timestamp en String
+            Date dateInsertion = (Date) row.get("poste_date_insertion");
+            posteDetail.setPosteDateInsertion(dateInsertion != null ? dateFormat.format(dateInsertion) : null);
+
+            // Conversion de BigDecimal en Double
+            BigDecimal salaire = (BigDecimal) row.get("poste_salaire");
+            posteDetail.setPosteSalaire(salaire != null ? salaire.doubleValue() : null);
+
+            posteDetail.setPosteTitre((String) row.get("poste_titre"));
+            posteDetail.setNbrAnneeExperience((Integer) row.get("nbr_annee_experience"));
+            posteDetail.setNbrAnneeFormation((Integer) row.get("nbr_annee_formation"));
+            posteDetail.setAgeRequise((Integer) row.get("age_requise"));
+            posteDetail.setIdDiplome((Long) row.get("id_diplome"));
+            posteDetail.setDiplomeNom((String) row.get("diplome_nom"));
+            posteDetail.setIdSecteur((Long) row.get("id_secteur"));
+            posteDetail.setSecteurNom((String) row.get("secteur_nom"));
+            posteDetail.setIdEntreprise((Long) row.get("id_entreprise"));
+            posteDetail.setEntrepriseNom((String) row.get("entreprise_nom"));
+            posteDetail.setEntreprisePoint((Integer) row.get("entreprise_point"));
+            posteDetail.setEntrepriseType((String) row.get("entreprise_type"));
+
+            // Conversion de java.sql.Date et java.sql.Timestamp en String
+            Date dateCreation = (Date) row.get("entreprise_date_creation");
+            posteDetail.setEntrepriseDateCreation(dateCreation != null ? dateFormat.format(dateCreation) : null);
+
+            posteDetail.setEntrepriseAdresse((String) row.get("entreprise_adresse"));
+            posteDetail.setEntrepriseSiteWeb((String) row.get("entreprise_site_web"));
+            posteDetail.setEntrepriseMail((String) row.get("entreprise_mail"));
+            posteDetail.setEntrepriseNumTelephone((String) row.get("entreprise_num_telephone"));
+            posteDetail.setEntrepriseLatitude((Double) row.get("entreprise_latitude"));
+            posteDetail.setEntrepriseLongitude((Double) row.get("entreprise_longitude"));
+            posteDetail.setEntrepriseImage((String) row.get("entreprise_image"));
+            posteDetail.setCompetenceRequise((String) row.get("competence_requise"));
+            results.add(posteDetail);
+        }
+
+        return results;
     }
-    
-    
 }
