cc5a78e5302ca61704d4db607c850562bde16c33
nekena123 <rakotomanjatonekena@gmail.com>
2024-06-22 12:09:39 +0200

    filtrage notification

--- src/main/java/itu/notification/NotificationJSONController.java
+++ src/main/java/itu/notification/NotificationJSONController.java
@@ -1,11 +1,14 @@
 package itu.notification;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import itu.utilisateur.Utilisateur;
 import jakarta.servlet.http.HttpSession;
 
 @RestController
@@ -17,5 +20,21 @@ public class NotificationJSONController {
     @GetMapping(value = "/notification/liste", produces = "application/json; charset=UTF-8")
     public List<Notification> listeNotification(HttpSession session) {
         return notificationRepository.findAll();
+    }
+
+    @GetMapping(value = "/notification/filter/{type}", produces = "application/json; charset=UTF-8")
+    public List<Notification> listeNotification(HttpSession session, @PathVariable("type") int type) {
+        List<Notification> notifications = new ArrayList<>();
+        Utilisateur utilisateur = (Utilisateur) session.getAttribute("utilisateur");
+        if (type==0) {
+            notifications = notificationRepository.findByUtilisateurIdAndPointTrue(utilisateur.getId());
+        }
+        if (type==1) {
+            notifications = notificationRepository.findByUtilisateurIdAndEntretienNotNull(utilisateur.getId());            
+        }
+        if (type == 2) {
+            notifications = notificationRepository.findByUtilisateurIdAndPosteNotNull(utilisateur.getId());
+        }
+        return notifications;
     }
 }
