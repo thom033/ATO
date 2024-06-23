4ede5d948aba8609254068ab1777780c29d72f89
Rakotoarimanana Tojo Tianh D. <tojorakotoarimanana01@gmail.com>
2024-06-23 14:40:48 +0300

    Upload image poste

--- src/main/java/itu/upload/FileUploadController.java
+++ src/main/java/itu/upload/FileUploadController.java
@@ -14,27 +14,22 @@ import java.nio.file.Paths;
 
 @RestController
 public class FileUploadController {
-    private static String UPLOAD_DIR = "src\\main\\webapp\\public\\img\\uploaded";
-    public static String getFileExtension(String fileName) {
-        if (fileName == null || fileName.lastIndexOf(".") == -1) {
-            return ""; // Retourne une chaîne vide si le nom de fichier est null ou ne contient pas de point
-        }
-        return fileName.substring(fileName.lastIndexOf(".") + 1);
-    }
-    @PostMapping("/upload")
-    public String uploadFile(@RequestParam("file") MultipartFile file ,HttpSession session) {
+    private static String UPLOAD_DIR_PROFIL= "src\\main\\webapp\\public\\img\\uploaded";
+
+    @PostMapping("/upload/profil")
+    public String uploadProfil(@RequestParam("file") MultipartFile file ,HttpSession session) {
         try {
             Utilisateur utilisateur = (Utilisateur) session.getAttribute("utilisateur");
 
             // Ensure the directory exists
-            File directory = new File(UPLOAD_DIR);
+            File directory = new File(UPLOAD_DIR_PROFIL);
             if (!directory.exists()) {
                 directory.mkdirs();
             }
 
             // Get the file and save it
             byte[] bytes = file.getBytes();
-            Path path = Paths.get(UPLOAD_DIR + File.separator + utilisateur.getId()+".jpg");
+            Path path = Paths.get(UPLOAD_DIR_PROFIL + File.separator + utilisateur.getId()+".jpg");
             Files.write(path, bytes);
 
             return "Image uploaded successfully!";
@@ -43,4 +38,25 @@ public class FileUploadController {
             return "Image upload failed!";
         }
     }
+    @PostMapping("/upload/poste/{posteId}")
+    public String uploadPoste(@RequestParam("file") MultipartFile file, @PathVariable("posteId") Long posteId) {
+        try {    
+            // Ensure the directory exists
+            File directory = new File(UPLOAD_DIR_PROFIL);
+            if (!directory.exists()) {
+                directory.mkdirs();
+            }
+    
+            // Get the file and save it
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get(UPLOAD_DIR_PROFIL + File.separator + "p_" + posteId+ ".jpg");
+            Files.write(path, bytes);
+    
+            return "Image uploaded successfully!";
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Image upload failed!";
+        }
+    }
+    
 }
--- src/main/webapp/public/js/upload.js
+++ src/main/webapp/public/js/upload.js
@@ -5,11 +5,32 @@ function uploadImage() {
     formData.append('file', file);
 
     var xhr = new XMLHttpRequest();
-    xhr.open('POST', '/upload', true);
+    xhr.open('POST', '/upload/profil', true);
 
     xhr.onload = function () {
         if (xhr.status === 200) {
             alert('Image uploaded successfully!');
+            location.reload();  // Reload the page
+        } else {
+            alert('Image upload failed!');
+        }
+    };
+
+    xhr.send(formData);
+}
+function uploadImagePoste(idPoste) {
+    var formData = new FormData();
+    var fileInput = document.getElementById('fileInput');
+    var file = fileInput.files[0];
+    formData.append('file', file);
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', '/upload/poste/' + idPoste, true);  // Use the idPoste in the URL
+
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            alert('Image uploaded successfully!');
+            location.reload();  // Reload the page
         } else {
             alert('Image upload failed!');
         }
