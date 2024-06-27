8ecd33a681ae86f59e3cb473baa1c8b65ce2172c
NicolasMaku <108606635+NicolasMaku@users.noreply.github.com>
2024-06-27 09:45:08 +0300

    maty
    maty

--- src/main/java/itu/poste/PosteJSONController.java
+++ src/main/java/itu/poste/PosteJSONController.java
@@ -8,16 +8,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-
 @RestController
 public class PosteJSONController {
     @Autowired
     PosteRepository posteRepository;
+
     @PostMapping("/poste/insert")
-    public String insert(@RequestBody Poste poste) throws Exception{
+    public String insert(@RequestBody Poste poste) throws Exception {
         poste.setDateInsertion(LocalDate.now());
-        //posteRepository.save(poste);
+        posteRepository.save(poste);
         return "{\"message\":\"inserted\"}";
     }
-    
+
 }
